b69ac2882c33aa8e33dffd1177c327e1bbf70b3d
user <kupaev.itrum@gmail.com>
2023-03-09 11:21:22 +0300

    fixed function name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const isIpAddressValidate = str => {
+const isValidIpAddress = str => {
   if (str.split('.').length !== 4) return false;
   for (const el of str.split('.')) {
     if (isNaN(el)) return false;
@@ -7,19 +7,19 @@ const isIpAddressValidate = str => {
   return true;
 } 
 
-console.log(isIpAddressValidate('255.255.255.255'));
-console.log(isIpAddressValidate('0.0.0.0'));
-console.log(isIpAddressValidate('192.168.0.1'));
-console.log(isIpAddressValidate('192.16.23.1'));
+console.log(isValidIpAddress('255.255.255.255'));
+console.log(isValidIpAddress('0.0.0.0'));
+console.log(isValidIpAddress('192.168.0.1'));
+console.log(isValidIpAddress('192.16.23.1'));
 
 console.log('/////////////////')
 
-console.log(isIpAddressValidate('192.168.0.1.'));
-console.log(isIpAddressValidate('192.168.0.q'));
-console.log(isIpAddressValidate('192.168.0 1'));
-console.log(isIpAddressValidate('12392.168.0.1'));
-console.log(isIpAddressValidate('192.16.123'));
-console.log(isIpAddressValidate('192.16821.-0123.1'));
-console.log(isIpAddressValidate('19216810123.1'));
-console.log(isIpAddressValidate(''));
-console.log(isIpAddressValidate('1qweqwr23r5tq23qewrf'));
+console.log(isValidIpAddress('192.168.0.1.'));
+console.log(isValidIpAddress('192.168.0.q'));
+console.log(isValidIpAddress('192.168.0 1'));
+console.log(isValidIpAddress('12392.168.0.1'));
+console.log(isValidIpAddress('192.16.123'));
+console.log(isValidIpAddress('192.16821.-0123.1'));
+console.log(isValidIpAddress('19216810123.1'));
+console.log(isValidIpAddress(''));
+console.log(isValidIpAddress('1qweqwr23r5tq23qewrf'));
