cd95c17dfac67f20da150fbf61c7f56a16001aa0
user <kupaev.itrum@gmail.com>
2023-03-09 14:53:07 +0300

    removed cross

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const images = [...document.getElementsByTagName('img')];
 const crosses = [...document.getElementsByClassName('cross')];
 
 let target = null;
+let closer = null;
 
 const close = () => {
   target.className = 'img';
+  closer.style = '';
 }
 
 const open = e => {
   target = e.target.parentNode;
+  closer = target.childNodes[1];
+  closer.style = 'position: absolute; top: 0; left: 0; right: 0; bottom: 0';
   target.className = 'img opened';
 }
 
